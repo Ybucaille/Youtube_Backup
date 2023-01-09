c3cd934fd02f281d215480a9591aa81d2fe55d6b
Yann Bucaille <bucaille.yann14@icloud.com>
2023-01-09 05:02:00 +0100

    main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
     # Créer un objet de la classe YouTubeDownloader
     yt = YouTubeDownloader()
-    # Appeler la fonction main de l'objet yt en passant les arguments de la ligne de commande
+    # Appelle la fonction main de l'objet yt en passant les arguments de la ligne de commande
     args = yt.parser.parse_args(sys.argv[1:])
     yt.main()
