753ca46fe6e9a032a8cfe0fdeae924f76702f356
Yann Bucaille <bucaille.yann14@icloud.com>
2023-01-10 11:52:16 +0100

    added the method backup

--- libs/youtube_backup.py
+++ libs/youtube_backup.py
@@ -24,10 +24,8 @@ class YouTubeDownloader:
         # Créer un parseur d'arguments
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument("-start", dest="vid", help="Start the program with an id of a video")
-        self.parser.add_argument("-downloadvid", help="Download a specific video, you have to put the index number of the video in the json file and to pass the json file with the --json argument")
-        self.parser.add_argument("-json", help="Download a specific video")
         #self.parser.add_argument("-numberofvideos", help="Get the number of videos of a channel")
-        self.parser.add_argument("-help", action="help", help="your json file with the video links")
+        self.parser.add_argument("-help", action="help", help="Open an help message")
 
 
     def main(self):
@@ -37,11 +35,7 @@ class YouTubeDownloader:
         # Appeler la fonction appropriée en fonction des arguments passés
         if self.args.vid:
             channel_id = self.get_channel_id(self.args)
-            self.get_channel_videos_url(channel_id, f'videos_{self.get_channel_title(channel_id)}.json')
-        elif self.args.downloadvid:
-            self.download_video(self.args.downloadvid)
-        elif self.args.json:
-            self.download_all_videos(self.args.json)
+            self.backup_videos(channel_id)
         elif self.args.help:
             self.parser.print_help()
 
@@ -158,43 +152,6 @@ class YouTubeDownloader:
         except:
             print("Quota exceeded")
 
-
-    def download_video(self, url):
-        """Fonction qui télécharge une vidéo YouTube à partir de son URL et la stocke dans le dossier spécifié"""
-        try:
-            #on récupère l'url de la vidéo à partir d'un fichier json
-            with open(self.args.json) as f:
-                data = json.load(f)
-                url = data[int(self.args.downloadvid)]
-            # Créer un objet YouTube
-            yt = YouTube(url)
-            # Récupérer la vidéo la plus haute résolution
-            video = yt.streams.get_highest_resolution()
-            # On récupère le path du dossier courant
-            path = os.getcwd()
-            # on vérifie si le dossier videos existe déjà dans le dossier courant
-            if not os.path.exists(os.path.join(path, 'videos')):
-                #créer un dossier videos dans le dossier courant
-                path = os.path.join(path, 'videos')
-                os.mkdir(path)
-            else:
-                path = os.path.join(path, 'videos')
-            # Envoyer une requête GET pour télécharger la vidéo
-            response = requests.get(video.url, stream=True)
-            # Ouvrir le fichier en mode écriture binaire
-            with open(os.path.join(path, video.title + '.mp4'), 'wb') as f:
-                # Télécharger la vidéo en chunks
-                with tqdm(total=int(response.headers.get('Content-Length', 0)), unit='B', unit_scale=True, desc=f'Downloading {video.title}') as pbar:
-                    for chunk in response.iter_content(chunk_size=1024):
-                        # Écrire le chunk dans le fichier
-                        f.write(chunk)
-                        # Mettre à jour la barre de progression
-                        pbar.update(len(chunk))
-            print(f'"la vidéo : {video.title}" a été téléchargé avec succès')
-        except Exception as e:
-            print(f'"{url}" n\'a pas pu être téléchargé')
-            print(e)
-
     def download_all_videos(self, videos_file):
         with open(videos_file, 'r') as file:
             videos_data = json.load(file)
@@ -228,3 +185,18 @@ class YouTubeDownloader:
                 except Exception as e:
                     print(f'"{url}" n\'a pas pu être téléchargé')
                     print(e)
+
+
+    def backup_videos(self, channel_id):
+        # On appelle la méthode get_channel_videos_url pour récupérer les vidéos de la chaîne en format json
+        self.get_channel_videos_url(channel_id, f'videos_{self.get_channel_title(channel_id)}.json')
+
+        # On récupère le path du dossier courant
+        path = os.getcwd()
+        # On récupère le nom du fichier json à l'aide de la méthode get_channel_title
+        filename = '\\jsons\\videos_{}.json'.format(self.get_channel_title(channel_id))
+        # On concatène le path du dossier courant avec le nom du fichier json
+        filepath = path + filename
+
+        # On appelle la méthode download_all_videos pour télécharger toutes les vidéos
+        self.download_all_videos(filepath)
